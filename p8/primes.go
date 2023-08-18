4f05e111c81bcdaeab300826a1d2c6bb051a33f0
dmitryshalavin <dmitry.shalavin@gmail.com>
2023-08-18 22:47:20 +0600

    primes

---
+++ p8/primes.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func Primes(n int) []int {
+	a := make([]bool, n+2)
+
+	for i := 2; i <= n; i++ {
+		if a[i] {
+			continue
+		}
+		for j := i * i; j <= n+1; j += i {
+			a[j] = true
+		}
+	}
+
+	fmt.Println(a)
+
+	res := []int{}
+	for i, v := range a[2:] {
+		if !v {
+			res = append(res, i+2)
+		}
+	}
+
+	return res
+}
+
+func main() {
+	fmt.Println(Primes(7))
+}
